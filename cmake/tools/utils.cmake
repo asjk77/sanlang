c7bbfd89e85656d59e69389f0b2d755cefaeef9f
asjk77 <39531831+asjk77@users.noreply.github.com>
2021-06-22 16:39:20 +0900

    Initial commit

---
+++ cmake/tools/utils.cmake
@@ -0,0 +1,9 @@
+################################################################################
+# File Name      : utils.cmake                                                 #
+# Created Date   : Friday, March 19th 2021, 10:39:22 am                        #
+# Author         : Hwang SanHo                                                 #
+# Email          : tksgh1000@naver.com                                         #
+# Copyright (c) 2021 San Form Co.                                              #
+################################################################################
+
+# Description :
---
+++ include/test.hpp
@@ -0,0 +1,16 @@
+/*******************************************************************************
+* File Name      : test.hpp                                                    *
+* Created Date   : Friday, March 19th 2021, 3:42:04 pm                         *
+* Author         : Hwang SanHo                                                 *
+* Email          : tksgh1000@naver.com                                         *
+* Copyright (c) 2021 San Form Co.                                              *
+*******************************************************************************/
+
+// Description :
+
+#ifndef INC_TEST_HPP
+#define INC_TEST_HPP
+
+void HelloWorld(void);
+
+#endif /* INC_TEST_HPP */
